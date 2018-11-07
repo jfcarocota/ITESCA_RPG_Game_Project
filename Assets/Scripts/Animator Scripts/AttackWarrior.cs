07f16300980075502bc811d7db7692faca5a61bd
TheMajesticBox <42950184+TheMajesticBox@users.noreply.github.com>
2018-11-07 14:44:39 -0700

    WarriorModification
    A few modification to the Warrior Class

--- Assets/Scripts/Animator Scripts/AttackWarrior.cs
+++ Assets/Scripts/Animator Scripts/AttackWarrior.cs
@@ -4,10 +4,28 @@ using UnityEngine;
 
 public class AttackWarrior : StateMachineBehaviour {
 
+    [SerializeField]
+
 	 //OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
 	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-	
-	}
+        //   Debug.Log("This object: " + this.name + " has started attacking");
+        //   Warrior.GetComponent<WarriorMan>().SetCollidersStatus(true, "Sword");
+            
+            if(animator.GetComponent<WarriorMan>() != null)
+            { 
+            WarriorMan warrior = animator.GetComponent<WarriorMan>();
+            warrior.SetCollidersStatus(true, "Sword");
+             }
+            else if (animator.GetComponent<WarriorDummy>())
+            {
+
+            WarriorDummy warrior2 = animator.GetComponent<WarriorDummy>();
+            warrior2.SetCollidersStatus(true, "Sword");
+            }
+       
+
+        //  Debug.Log("Padre: " + this);
+    }
 
 	// OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
 	override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
@@ -16,11 +34,24 @@ public class AttackWarrior : StateMachineBehaviour {
 
 	// OnStateExit is called when a transition ends and the state machine finishes evaluating this state
 	override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-	
-	}
+        //   Warrior.GetComponent<WarriorMan>().SetCollidersStatus(false, "Sword");
+        //   Debug.Log("This object: " + this.name + " has finished attacking");
+            if (animator.GetComponent<WarriorMan>() != null)
+            {
+                WarriorMan warrior = animator.GetComponent<WarriorMan>();
+                warrior.SetCollidersStatus(false, "Sword");
+            }
+            else if (animator.GetComponent<WarriorDummy>())
+            {
 
-	// OnStateMove is called right after Animator.OnAnimatorMove(). Code that processes and affects root motion should be implemented here
-	override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+                WarriorDummy warrior2 = animator.GetComponent<WarriorDummy>();
+                warrior2.SetCollidersStatus(false, "Sword");
+            }
+
+    }
+
+    // OnStateMove is called right after Animator.OnAnimatorMove(). Code that processes and affects root motion should be implemented here
+    override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
 	
 	}
 
--- Assets/Scripts/Characters/Warrior/Shield.cs
+++ Assets/Scripts/Characters/Warrior/Shield.cs
@@ -20,10 +20,7 @@ public class Shield : MonoBehaviour
     {
 
 
-     //   Debug.Log("Shield");
-        //Debug.Log("SCollider: " + other.name + " Padre: " + other.transform.root.name + " Objeto tocado: " + this.name);
-       // Debug.Log("PCollider padre: " + other.transform.root.name + " Padre de este: " + this.transform.root.name );
-
+    
         string padre = other.transform.root.name;
         string padreeste = this.transform.root.name;
         WarriorMan thisone = null;
@@ -33,56 +30,52 @@ public class Shield : MonoBehaviour
         WarriorDummy otherwise = null;
         if (padreeste.StartsWith("WarriorD") )
         {
-          //  Debug.Log("Please work este1");
            thatone = this.transform.root.gameObject.GetComponent<WarriorDummy>();
 
         }
         else if (padreeste.StartsWith("Warrior") )
         {
-            //Debug.Log("Please work este2");
             thisone = this.transform.root.gameObject.GetComponent<WarriorMan>();
 
         }
 
         if (padre.StartsWith("WarriorD"))
         {
-           // Debug.Log("Please work padre1");
             otherwise = other.transform.root.gameObject.GetComponent<WarriorDummy>();
 
         }
         else if (padre.StartsWith("Warrior"))
         {
-          //  Debug.Log("Please work padre2");
-            otherone = this.transform.root.gameObject.GetComponent<WarriorMan>();
+            otherone = other.transform.root.gameObject.GetComponent<WarriorMan>();
 
         }
 
         if (other.tag == "Damage" && (this.transform.root.name != other.transform.root.name))
         {
-            Debug.Log("El escudo ha sido golpeado por algo que hace daño");
-            Debug.Log("WarriorMan: " + otherwise + ":" + otherone + " DummyMan: " + thatone + ":" + thisone);
+        //    Debug.Log("The shield has been hit by something that does damage");
+         //   Debug.Log("WarriorMan: " + otherwise + ":" + otherone + " DummyMan: " + thatone + ":" + thisone);
 
             if(thatone != null)
             {
                 if (thatone.Guarded)
                 {
-                    Debug.Log("Golpe anulado men"); 
+                    Debug.Log("Hit has been Shielded"); 
                     otherone.SetCollidersStatus(false, "Sword");
                 }
-            }
+            } 
+            
             if(thisone != null)
             {
                 if (thisone.Guarded)
                 {
-                    Debug.Log("Golpe anulado men");
+                    Debug.Log("Hit has been Shielded");
                     otherwise.SetCollidersStatus(false, "Sword");
                 }
             }
             
 
 
-        }
-        
+        }        
        
     }
     private void OnCollisionExit(Collision collision)
