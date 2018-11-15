ceb48eb6844fa001585ecea88794980a3e8b2dcd
TheMajesticBox <42950184+TheMajesticBox@users.noreply.github.com>
2018-11-14 18:54:23 -0700

    Cosillas
    CosillasDelCannon

---
+++ Assets/Scripts/Props/Cannon/Cannon.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GameCore.SystemControls;
+using GameCore.ObjectPooler;
+
+public class Cannon : MonoBehaviour {
+
+    [SerializeField]
+    GameObject ballSpawner;
+    ObjectPooler objectPooler;
+    public float attackValue;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Shoot();
+	}
+
+    void Shoot()
+    {
+        if (Controllers.GetFire(1, 2))
+        {
+            
+                
+                objectPooler.GetObjectFromPool("Spell", ballSpawner.transform.position, ballSpawner.transform.rotation, null);
+                objectPooler.GetObjectFromPool("SpellCast", ballSpawner.transform.position, ballSpawner.transform.rotation, ballSpawner.transform);
+            
+        }
+
+
+
+    }
+}
